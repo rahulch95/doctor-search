41df64a9020dc357da5dc46d904499baafe0f9e0
Rahul Chaudhary <rahul.chaudhary@mail.utoronto.ca>
2015-09-27 02:16:42 -0400

    changes

---
+++ getMapping.py
@@ -0,0 +1,8 @@
+SPECIALTYUIDMAPPING = {
+    'allergy': 'allergist',
+    'dietitian': 'dietitian',
+    'cardiologist': 'cardiologist'
+}
+
+def getSpecialtyUidMapping(specialty):
+    return SPECIALTYUIDMAPPING['specialty']
