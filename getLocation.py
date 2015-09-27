b7c021b11b3f680d519f78bb6be84f6f1cedd824
Rahul Chaudhary <rahul.chaudhary@mail.utoronto.ca>
2015-09-27 02:10:48 -0400

    changes

---
+++ getLocation.py
@@ -0,0 +1,6 @@
+def get_location(phone_number):
+    return ["37.773", "-122.413", 50]
+
+
+def get_user_location(phone_number):
+    return ["37.773", "-122.413"]
--- server.py
+++ server.py
@@ -1,9 +1,35 @@
 import os
-from flask import Flask
+from flask import Flask, request
+import getMapping
+import getLocation
+import requests
+import json
 
+secret = os.environ.get('betterdoctor-apikey', None)
 app = Flask(__name__)
-
+URL = "https://api.betterdoctor.com/2014-09-12/"
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/api/search-specialty', methods=['POST'])
+def search_specialty():
+    specialty = request.form['specialty']
+    phone_number = request.form['phone_number']
+    data = {
+        'specialty_uid': getMapping.getSpecialtyUidMapping(specialty),
+        'user_key': secret,
+        'location': ",".join(getLocation.get_location(phone_number)),
+        'user_location': ",".join(getLocation.get_user_location(phone_number)),
+        'limit': 3,
+        'sort': 'rating-desc'
+    }
+    resp = requests.post(URL + 'doctor', data)
+    if resp.status_code == 200:
+        data = json.loads(resp.text)
+        print(data)
+        return resp.text
+    return resp
+
+
