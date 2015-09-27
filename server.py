527ec77d1dba0f1eb8cd87c308b3366e21a76d6d
Rahul Chaudhary <rahul.chaudhary@mail.utoronto.ca>
2015-09-27 04:23:41 -0400

    changes

--- server.py
+++ server.py
@@ -10,6 +10,38 @@ secret = os.environ.get('betterdoctor-apikey', None)
 app = Flask(__name__)
 URL = "https://api.betterdoctor.com/2015-01-27/"
 
+
+class Doctor:
+    def __init__(self):
+        self.name = None
+        self.practice = None
+        self.address = None
+        self.uid = None
+        self.phone_number = None
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "practice": self.practice,
+            "uid": self.uid,
+            "address": self.address,
+            "phone_number": self.phone_number
+        }
+
+    def set_address(self, visit_address):
+        address = []
+        if visit_address['street']:
+            address.append(visit_address['street'])
+        if visit_address['city']:
+            address.append(visit_address['city'])
+        if visit_address['state']:
+            address.append(visit_address['state'])
+        if visit_address['zip']:
+            address.append(visit_address['zip'])
+
+        self.address = ", ".join(address)
+
+
 @app.route('/')
 def hello():
     return 'Hello World!'
@@ -30,17 +62,26 @@ def search_specialty():
     }
     print(URL + 'doctors')
     resp = requests.get(URL + 'doctors', data)
-    print(resp)
-    print(resp.status_code)
-    print(resp.text)
     logging.info(resp)
-    logging.error(resp)
     if resp.status_code == 200:
-
         data = json.loads(resp.text)
-        print(data)
-        return resp.text
-    return resp
+        i = 1
+        response = {}
+        for doctor_data in data["data"]:
+            new_doctor = Doctor()
+            practices = doctor_data["practices"]
+            for practice in practices:
+                if practice['within_search_area']:
+                    new_doctor.practice = practice['name']
+                    new_doctor.set_address(practice['visit_address'])
+                    for phone in practice['phones']:
+                        new_doctor.phone_number = phone['number']
+                        break
+                    break
+            profile = doctor_data['profile']
+            new_doctor.name = profile['first_name'] + ' ' + profile['last_name']
+            response[i] = new_doctor.to_dict()
+            i += 1
 
-
-search_specialty()
+        return 200, json.dumps(response)
+    return 400, {}
